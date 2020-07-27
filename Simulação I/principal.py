a09e972587bfae602789a4580792926cc12b2ff8
lucasfauster <lucasfauster@hotmail.com>
2020-07-27 18:52:45 -0300

    organizando

--- Simulação I/principal.py
+++ Simulação I/principal.py
@@ -1,28 +1,9 @@
-
-'''from sklearn.cluster import KMeans
-import pandas as pd
-import numpy as np
-from yellowbrick.cluster import KElbowVisualizer
-df = pd.read_csv("../matriz-adj.csv")
-df = df.drop(columns='Unnamed: 0')
-model = KMeans()
-visualizer = KElbowVisualizer(model, k=(1,10))
-visualizer.fit(self.dataset)
-visualizer.show()
-self.dataset = True
-
-a = {1: 'a'}
-print(a[1])'''
-
-
-from simulador import Simulador
+import simulador
 import graph
 import node
 import usuario
 import clusterizador as cls
 import csv
-
-
 
 def lerArq(n):
     leitorArq=[]
@@ -32,8 +13,6 @@ def lerArq(n):
         for linha in csvReader:
             leitorArq.append(linha)
     return leitorArq
-
-
 
 def criandoNode(mat):
     listaN=[]
@@ -46,13 +25,10 @@ def criandoNode(mat):
                 if mat[0][j]==listaN[j-1].id:
                     listaN[j-1].vizinhos.append(listaN[i-1])
 
-
 def createElbow(dir):
     cluster = cls.Clusterizador(dir)
     cluster.abrirDataSet()
     cluster.gerarGrafico()
-
-
 
 '''def alocaInfoDataset():
     campos=[]
@@ -60,22 +36,13 @@ def createElbow(dir):
         for i in range(len(leitorArq)):
             print(leitorArq[j][i])'''
 
-
-
 grafo= graph.Graph()
 matrizAdj= lerArq("matriz-adj.csv")
 
 criandoNode(matrizAdj)
 grafo.createArcs()
 
-
-cluster = cls.Clusterizador("matriz-adj.csv")
-cluster.abrirDataSet()
-cluster.gerarClusters()
-
-
 simula=simulador.Simulador(0,grafo)
 f=open("dataset-filtrado(1).csv", newline='')
 dataset=csv.reader(f)
 simula.realizaSimulacao(dataset)
-
