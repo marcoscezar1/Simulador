7bec4be092bbaeda1e7669bd7bb4bc722d07a0ee
Belato <42917518+Belato@users.noreply.github.com>
2020-07-29 18:36:51 -0300

    Update usuario.py

--- Simulação I/usuario.py
+++ Simulação I/usuario.py
@@ -1,10 +1,11 @@
 class Usuario:
-    def __init__(self, id, tempo_de_chegada, tempo_de_saida):
+    def __init__(self, id, tempo_de_chegada, tempo_de_saida, ap):
         self.id = id
         self.tempo_de_chegada = tempo_de_chegada
         self.tempo_de_saida = tempo_de_saida
         self.node_associado = None
         self.conectado = False
+        self.ap_preferencial=ap
 
     def adcNode(self, node):
         if(self.node_associado == None):
@@ -25,6 +26,6 @@ class Usuario:
 
 
     def mudarTemposInOut(self, tempoChegada, tempoSaida):
-        if tempoSaida>=self.tempo_de_saida:
-            self.tempo_de_chegada=tempoChegada
-            self.tempo_de_saida=tempoSaida
+
+        self.tempo_de_chegada=tempoChegada
+        self.tempo_de_saida=tempoSaida
