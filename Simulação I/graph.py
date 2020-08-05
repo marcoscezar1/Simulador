739da759b3bc81bb0afd0ba986aef02ec690125c
Belato <42917518+Belato@users.noreply.github.com>
2020-08-05 20:46:45 -0300

    Update graph.py

--- Simulação I/graph.py
+++ Simulação I/graph.py
@@ -2,6 +2,7 @@ class Graph:
     def __init__(self):
         self.nodes = []
         self.arcs = []
+        self.nao_servido = 0
 
     def createNode(self, node):
         self.nodes.append(node)
@@ -11,3 +12,6 @@ class Graph:
             for j in i.vizinhos:
                 if ((i.id, j.id) not in self.arcs) and((j.id, i.id) not in self.arcs):
                     self.arcs.append((i,j))
+
+    def increment_nao_servido(self):
+        self.nao_servido += 1
