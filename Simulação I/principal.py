203453f43becb7e1d6e2d1084e14d6da97c58485
Belato <42917518+Belato@users.noreply.github.com>
2020-08-05 20:43:02 -0300

    Update principal.py

--- Simulação I/principal.py
+++ Simulação I/principal.py
@@ -14,6 +14,16 @@ def lerArq(n):
             leitorArq.append(linha)
     return leitorArq
 
+def ArqNumLinha(n):
+    qtdLinhas=0
+    with open(n) as csvFile:
+        csvReader = csv.reader(csvFile, delimiter=',')
+        for linha in csvReader:
+            qtdLinhas+=1
+    return qtdLinhas
+
+
+
 def criandoNode(mat):
     listaN=[]
     for j in range(1, len(mat[0])):
@@ -44,6 +54,8 @@ grafo.createArcs()
 APsIniciais.inicializaVizinhos(grafo)
 
 simula=simulador.Simulador(0,grafo)
+numLinhas=ArqNumLinha("dataset-filtrado(1).csv")
 f=open("dataset-filtrado(1).csv", newline='')
 dataset=csv.reader(f)
-simula.realizaSimulacao(dataset)
+simula.realizaSimulacao(dataset, numLinhas)
+f.close()
